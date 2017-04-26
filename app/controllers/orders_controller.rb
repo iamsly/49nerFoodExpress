f1591cdcc40151ecbee66c388bb3179335fd90a1
mjhighto <mjhighto@uncc.edu>
2017-04-26 12:16:21 +0000

    Style errors and messages partial

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
  def update
      
      if @order.update(order_params)
-          flash[:notice] = "Order was successfully updated"
+          flash[:success] = "Order was successfully updated"
           redirect_to order_path(@order)
      else
          render 'edit'
@@ -26,7 +26,7 @@ class OrdersController < ApplicationController
  def create
     @order = Order.new(order_params)
     if @order.save
-         flash[:notice] = "Order was successfully created"
+         flash[:success] = "Order was successfully created"
          redirect_to order_path(@order)
     else
         render 'new'
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
  def destroy
      
      @order.destroy
-     flash[:notice] = "Order item was removed from history"
+     flash[:danger] = "Order item was removed from history"
      redirect_to orders_path(@order)
  end
  
