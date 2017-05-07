75db6d7ba8a462f1dca8f44b160003df46eb1a8e
mjhighto <mjhighto@uncc.edu>
2017-05-07 10:08:09 +0000

    Add user password-Sylvester Mensah

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419134250) do
+ActiveRecord::Schema.define(version: 20170507100042) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "item"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170419134250) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
