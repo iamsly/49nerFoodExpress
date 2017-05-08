bdba207d1f33e7bd9f7914e857f5e2e4423eff5e
Sylvester Mensah <smensah1@uncc.edu>
2017-05-08 13:12:40 +0000

    add menu and chnaged name of folder

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
     belongs_to :user
     validates :item, presence: true, length: {minimum: 3, maximum: 10}
-    validates :drink, presence: true, length: {minimum: 3, maximum: 50}
-    validates :room, presence: true, length: {minimum: 3, maximum: 10}
+    validates :drink, presence: true, length: {minimum: 3, maximum: 100}
+    validates :room, presence: true, length: {minimum: 3, maximum: 500}
     validates :description, presence: true, length: {minimum: 3, maximum: 500}
     validates :user_id, presence: true
 end
