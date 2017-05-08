a683b5ce99bc9a6879685a4a6e1bed3d99cce20d
Sylvester Mensah <smensah1@uncc.edu>
2017-05-08 11:25:59 +0000

    add sign in message when you signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+             session[:user_id] = @user.id
             flash[:success] = "Welcome to 49ner Food Express #{@user.username}"
-            redirect_to orders_path
+             redirect_to user_path(@user)
         else
             render 'new'
         end
